9b4f23b73b4d0f670ca5c27869c0d9599231dffb
Hasan <hsmusayev@gmail.com>
2023-06-06 11:29:18 +0400

    VueJs-Laravel Amazon Clone

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(int $id)
+    {
+        return Inertia::render('Product', [
+            'product' => Product::find($id),
+        ]);
+    }
+
+}
